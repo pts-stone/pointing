2d557860394da2953f4d1412e576a9dcc0539bba
Stone Chao <stone.chao@points.com>
2017-04-20 18:13:39 -0400

    data transform

--- js/main.js
+++ js/main.js
@@ -6,13 +6,40 @@ const chart = new GoogleChartsMap("map");
 
 // create a function to subscribe to topics
 const subscriber = function (msg, data) {
-    chart.add(data);
+    console.log(data)
 };
 
 //Returns a random integer between min (inclusive) and max (inclusive)
 const getRandomInt =function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+
+//shuffle the array randomly
+const shuffle = function(array){
+    return _.shuffle(array);
+};
+
+const transform = function(order){
+    const payment = order.value.data.payment;
+    const orderDetails = order.value.data.orderDetails;
+    const coordinate = latlong[payment.billingInfo.country.toUpperCase()];
+    var bubble = {
+        latitude: 43.650391,
+        longitude: -79.383938,
+        address: "unknown",
+        lp_name: "globalrewards",
+        points: 100,
+        cost: 100,
+        timestamp: new Date().getTime()
+    };
+    bubble.latitude = coordinate.latitude;
+    bubble.longitude = coordinate.longitude;
+    bubble.cost = payment.costs.totalCost;
+    bubble.points = orderDetails.basePoints + (typeof orderDetails["bonusPoints"] == 'Number' ? orderDetails["bonusPoints"] : 0)
+    bubble.address = [payment.billingInfo.street1, payment.billingInfo.city, payment.billingInfo.state].join();
+    bubble.lp_name = orderDetails.loyaltyProgram;
+    return bubble;
+};
 
 // add the function to the list of subscribers for a particular topic
 // we're keeping the returned token, in order to be able to unsubscribe
@@ -21,26 +48,21 @@ var token = PubSub.subscribe(POINTING, subscriber);
 
 // publish a topic syncronously
 function update() {
-    var orders = [];
-    console.log(orders.length);
-    const times = getRandomInt(1, orders.length < 20 ? orders.length : 20 );
-    _.times(times, function(){
-        orders.push({
-            address: "19 Great Oak Dr",
-            lp_name: "globalrewards",
-            points: 100,
-            cost: 100,
-            timestamp: now
-        })
-    });
-    PubSub.publishSync(POINTING, {
-        address: "19 Great Oak Dr",
-        lp_name: "globalrewards",
-        points: 100,
-        cost: 100,
-        timestamp: now
-    });
-}
+    var bubbles = [];
+    var orders = shuffle(data["rows"]);
+    const size = orders;
+    console.log(size);
+    const times = getRandomInt(1, orders < 20 ? size : 20 );
+
+    for(var i = 0; i < times; i++){
+        try{
+            bubbles.push(transform(orders[i]));
+        } catch(e){
+            console.error('transform', ex.message);
+        }
+    }
+    PubSub.publishSync(POINTING, bubbles);
+};
 
 
 google.charts.load('current', {'packages': ['geochart']});
