7ef298a2d3bde49d4d1fcb0f978690a5dcc99157
Stone Chao <stone.chao@points.com>
2017-04-20 16:51:13 -0400

    process orders

--- js/main.js
+++ js/main.js
@@ -5,24 +5,45 @@ const POINTING = 'Pointing';
 const chart = new GoogleChartsMap("map");
 
 // create a function to subscribe to topics
-const subscriber = function ( msg, data ){
+const subscriber = function (msg, data) {
     chart.add(data);
 };
 
-
+//Returns a random integer between min (inclusive) and max (inclusive)
+const getRandomInt =function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // add the function to the list of subscribers for a particular topic
 // we're keeping the returned token, in order to be able to unsubscribe
 // from the topic later on
-var token = PubSub.subscribe( POINTING, subscriber );
+var token = PubSub.subscribe(POINTING, subscriber);
 
 // publish a topic syncronously
-function update(){
-    const milliseconds = new Date().getTime();
-    var now = Date.now();
-    PubSub.publishSync( POINTING, {address: "19 Great Oak Dr", lp_name: "globalrewards", points: 100, cost: 100, timestamp: now} );
+function update() {
+    var orders = [];
+    console.log(orders.length);
+    const times = getRandomInt(1, orders.length < 20 ? orders.length : 20 );
+    _.times(times, function(){
+        orders.push({
+            address: "19 Great Oak Dr",
+            lp_name: "globalrewards",
+            points: 100,
+            cost: 100,
+            timestamp: now
+        })
+    });
+    PubSub.publishSync(POINTING, {
+        address: "19 Great Oak Dr",
+        lp_name: "globalrewards",
+        points: 100,
+        cost: 100,
+        timestamp: now
+    });
 }
 
 
 google.charts.load('current', {'packages': ['geochart']});
 google.charts.setOnLoadCallback(() => setInterval(update, 500));
+
+
