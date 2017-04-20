8c1356e6d27ea0c8ac71dd9b9faa0999e7b9804c
Stone Chao <stone.chao@points.com>
2017-04-20 15:22:42 -0400

    pub/sub pattern

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 /**
  * Created by stone.chao on 20/04/17.
  */
-function handler(){
+
+const POINTING = 'Pointing';
+
+// create a function to subscribe to topics
+const subscriber = function ( msg, data ){
+    document.getElementById("map").innerHTML = data;
+};
+
+// add the function to the list of subscribers for a particular topic
+// we're keeping the returned token, in order to be able to unsubscribe
+// from the topic later on
+var token = PubSub.subscribe( POINTING, subscriber );
+
+// publish a topic syncronously
+function update(){
     const milliseconds = new Date().getTime();
-    document.getElementById("map").innerHTML = "hello world! Now is " + milliseconds;
+    PubSub.publishSync( POINTING, 'hello world! Now is '+ milliseconds );
 }
-setInterval(handler, 500);
+
+//trigger the update per 0.5 second
+setInterval(update, 500);
