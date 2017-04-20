ec824ac8e6545de9755fde5c8f3eb07e3ca7a39e
Stone Chao <stone.chao@points.com>
2017-04-20 14:12:53 -0400

    hello world

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 /**
  * Created by stone.chao on 20/04/17.
  */
-document.getElementById("map").innerHTML = "hello word!";
+function connect(){
+    document.getElementById("map").innerHTML = "hello word!";
+}
+
+connect();
